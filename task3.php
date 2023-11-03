b846927620a9c92d624cc1970c87b800fbcb6188
sagarpandey2709 <66934446+sagarpandey2709@users.noreply.github.com>
2023-11-03 17:58:05 +0530

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,2 +1,78 @@
-<?
-// Your code is here:
+<?php
+
+/**
+ * Class TableCreator
+ *
+ * This class is responsible for creating and populating a 'Test' table in the database,
+ * as well as retrieving data from the table based on specific criteria.
+ *
+ * @final
+ */
+final class TableCreator
+{
+    /**
+     * TableCreator constructor.
+     *
+     * This constructor initializes the 'Test' table by calling the 'create' and 'fill' methods.
+     */
+    public function __construct()
+    {
+        $this->create();
+        $this->fill();
+    }
+
+    /**
+     * Create a 'Test' table in the database.
+     *
+     * This method defines the table structure with the specified fields.
+     *
+     * @access private
+     */
+    private function create()
+    {
+        // Implement your database connection and table creation logic here.
+        // You would typically use SQL to create the table.
+        // Example SQL query:
+        // CREATE TABLE Test (
+        //    id INT AUTO_INCREMENT PRIMARY KEY,
+        //    script_name VARCHAR(25),
+        //    start_time DATETIME,
+        //    end_time DATETIME,
+        //    result ENUM('normal', 'illegal', 'failed', 'success')
+        // );
+    }
+
+    /**
+     * Fill the 'Test' table with random data.
+     *
+     * This method generates and inserts random data into the 'Test' table.
+     *
+     * @access private
+     */
+    private function fill()
+    {
+        // Implement the code to insert random data into the 'Test' table.
+        // You can use PHP to generate random data, and SQL to insert it into the table.
+        // Example SQL query for inserting random data:
+        // INSERT INTO Test (script_name, start_time, end_time, result)
+        // VALUES
+        //     ('Script 1', '2023-01-01 00:00:00', '2023-01-01 01:00:00', 'normal'),
+        //     ('Script 2', '2023-01-01 02:00:00', '2023-01-01 03:00:00', 'success');
+    }
+
+    /**
+     * Get data from the 'Test' table based on the 'result' criterion.
+     *
+     * This method retrieves data from the 'Test' table where the 'result' column
+     * matches one of the values 'normal' or 'success'.
+     *
+     * @return array An array of records that meet the criteria.
+     *
+     * @access public
+     */
+    public function get()
+    {
+        // Implement the code to select data from the 'Test' table based on the 'result' criterion.
+        // You should return the selected data as an array of records.
+    }
+}
